1453d1147a7eecab77914194c1703708db733901
Evan Ngugi <evanngugi547@gmail.com>
2024-06-16 13:03:42 +0300

    first commit

--- sliders/slider-00/main.js
+++ sliders/slider-00/main.js
@@ -1,1 +1,14 @@
-console.log('Hello World!');
+const nextBtn=document.querySelector(".next");
+const prevBtn=document.querySelector(".prev");
+ const listDom=document.querySelector(".carousel .list");
+ const thumbnailsDom=document.querySelector(".carousel .thumbnails");
+
+nextBtn.onclick=function nextImage(){
+    let itemSlider=document.querySelectorAll(".carousel .list .item");  
+    let thumbnailItem=document.querySelectorAll(".carousel  .thumbnail");
+
+    listDom.appendChild(itemSlider[0]);
+    thumbnailsDom.appendChild(thumbnailItem[0]);
+
+}
+console.log("hello")
