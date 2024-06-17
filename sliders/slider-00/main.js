d592c30d08cf8e70846d715eb64dd731df398c3d
Evan Ngugi <evanngugi547@gmail.com>
2024-06-17 17:03:45 +0300

    add second commit contains some bugs need fixing

--- sliders/slider-00/main.js
+++ sliders/slider-00/main.js
@@ -1,14 +1,18 @@
-const nextBtn=document.querySelector(".next");
-const prevBtn=document.querySelector(".prev");
- const listDom=document.querySelector(".carousel .list");
- const thumbnailsDom=document.querySelector(".carousel .thumbnails");
+let  nextBtn=document.querySelector(".next");
+let prevBtn=document.querySelector(".prev");
+let  carouselDom=document.querySelector(".carousel")
+let listDom=document.querySelector(".carousel .list");
+let thumbnailsDom=document.querySelector(".carousel .thumbnails");
 
-nextBtn.onclick=function nextImage(){
+nextBtn.onclick=()=>{
+    nextImage("next")
+}
+function nextImage(type){
     let itemSlider=document.querySelectorAll(".carousel .list .item");  
     let thumbnailItem=document.querySelectorAll(".carousel  .thumbnail");
-
+    if(type==="next"){
     listDom.appendChild(itemSlider[0]);
     thumbnailsDom.appendChild(thumbnailItem[0]);
-
+    carouselDom.classList.add("next")
 }
-console.log("hello")
+}
